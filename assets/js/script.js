0b90fe4b9e440d58985dca8b2a69347764a4236f
Rafael nunes <studiosafeway@gmail.comcd>
2019-06-13 18:24:42 -0300

    ,,,

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 window.onload = function PegarDias() {
+    //Funcao para mostrar os Dias da Semana 
     var d = new Date();
     var weekday = new Array(7);
     weekday[0] = "Domingo";
@@ -9,13 +10,17 @@ window.onload = function PegarDias() {
     weekday[5] = "Sexta";
     weekday[6] = "Sabado";
   
+    //
     var n = weekday[d.getDay()];
+    //
     document.getElementById("dayWeek").innerHTML = n;
+    document.getElementById("dayWeek2").innerHTML = n;
 
 
-    
+    //Funcao para mostrar o Dia do Mes 
         var d = new Date();
         var m = d.getDate();
         document.getElementById("dayMonth").innerHTML = m;
+        document.getElementById("dayMonth2").innerHTML = m;
       
   }
