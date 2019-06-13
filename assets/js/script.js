3f8c14e57dce56421eb0dee8587fed02272890ee
Rafael nunes <studiosafeway@gmail.comcd>
2019-06-13 16:32:41 -0300

    Projeto nao finalizado

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+window.onload = function myFunction() {
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Domingo";
+    weekday[1] = "Segunta";
+    weekday[2] = "Terca";
+    weekday[3] = "Quarta";
+    weekday[4] = "Quinta";
+    weekday[5] = "Sexta";
+    weekday[6] = "Sabado";
+  
+    var n = weekday[d.getDay()];
+    document.getElementById("dayWeek").innerHTML = n;
+
+
+    
+        var d = new Date();
+        var m = d.getDate();
+        document.getElementById("dayMonth").innerHTML = m;
+      
+  }
