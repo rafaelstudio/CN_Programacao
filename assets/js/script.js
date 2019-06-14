0f5d336768cd86f94a54c3e1df3f62988296ce36
Rafael nunes <studiosafeway@gmail.comcd>
2019-06-14 18:33:56 -0300

    Comecei a desenvolver o model2 porem nao finalizei

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,19 @@ window.onload = function PegarDias() {
       
   }
 
+  function IniciaModal(modalID){
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == modalID || e.target.className == 'fechar'){
+            modal.classList.remove('mostrar');
+        }
+    });
+}
+
+const proxDesenho = document.querySelector('.proxDesenho');
+proxDesenho.addEventListener('click', ()=> IniciaModal('modal-desenho'));
+
 
 
   
